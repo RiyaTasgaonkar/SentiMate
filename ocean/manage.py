db703e783c0230c0c235f041109f1f1576dd4a3d
RiyaTasgaonkar <riyatasgaonkar@gmail.com>
2021-04-01 15:20:24 +0530

    django setup

--- ocean/manage.py
+++ ocean/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SentiMate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocean.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- ocean/ocean/wsgi.py
+++ ocean/ocean/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SentiMate project.
+WSGI config for ocean project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SentiMate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocean.settings')
 
 application = get_wsgi_application()
