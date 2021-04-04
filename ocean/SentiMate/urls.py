5e4088d143cf63e96a61b1e9cbc2defcffe73f8d
RiyaTasgaonkar <riyatasgaonkar@gmail.com>
2021-04-04 12:04:50 +0530

    added login and registration

--- ocean/SentiMate/urls.py
+++ ocean/SentiMate/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
+from django.contrib.auth import views as authViews
 from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('register',views.register, name='register'),
+    path('login',authViews.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', views.logout, name='logout'),
+    path('tests',views.tests, name='tests'),
 ]
--- ocean/SentiMate/views.py
+++ ocean/SentiMate/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def register(request):
+    if request.method =='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Account Created Successfully!')
+            return redirect('login')
+        else:
+            messages.info(request,'Account creation be failed :(')
+    else:
+        form=UserCreationForm()
+    return render(request,'register.html', {'form':form})
+
+@login_required
+def tests(request):
+    user=request.user.username
+    return render(request, 'tests.html', {'user':user})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.info(request,'You have been logged out.')
+    return redirect('home')
