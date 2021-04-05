f875a8c53c400cfe51aa8b58451058457c559c13
RiyaTasgaonkar <riyatasgaonkar@gmail.com>
2021-04-05 19:46:17 +0530

    fixed messages and added routes

--- ocean/SentiMate/urls.py
+++ ocean/SentiMate/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('register',views.register, name='register'),
     path('login',authViews.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.logout_view, name='logout'),
     path('tests',views.tests, name='tests'),
     path('profile',views.profile, name='profile'),
     path('testB',views.testB, name='testB'),
